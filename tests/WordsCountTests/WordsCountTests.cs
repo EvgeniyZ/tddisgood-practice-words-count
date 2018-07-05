f95086221eb4b5700ccd923239f233c1bbe0c02d
Evgenii Zakharov <Evgenii_Zakharov@epam.com>
2018-07-05 10:17:50 +0300

    add more test cases for WordsCount

--- tests/WordsCountTests/WordsCountTests.cs
+++ tests/WordsCountTests/WordsCountTests.cs
@@ -8,6 +8,9 @@ namespace WordsCountTests
         [Theory]
         [InlineData("", 0)]
         [InlineData("solve", 1)]
+        [InlineData("TDD", 1)]
+        [InlineData("I", 1)]
+        [InlineData("I'm", 1)]
         public void WordsCountTest(string line, int expectedCount)
         {
             Assert.Equal(expectedCount, WordsCount(line));
